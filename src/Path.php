6eea2776b64ae14d423fd6344691a33a2b02cef9
Samgu Lee <cable8mm@gmail.com>
2024-03-10 04:27:26 +1300

    Add deployment to GitHub Page (#8)
    * Add deployment to GitHub Page
    
    * Update .gitignore to remove composer.lock
    
    * Standardize github actions for me
    
    * Create files to integrate
    
    * Fix a symfony/console version to pass github actions

--- src/Path.php
+++ src/Path.php
@@ -4,22 +4,22 @@ namespace Cable8mm\QrImages;
 
 class Path
 {
-    public static function root()
+    public static function root(): string
     {
         return getcwd().DIRECTORY_SEPARATOR;
     }
 
-    public static function resources()
+    public static function resources(): string
     {
         return self::root().'resources'.DIRECTORY_SEPARATOR;
     }
 
-    public static function export()
+    public static function export(): string
     {
         return self::resources().'export'.DIRECTORY_SEPARATOR;
     }
 
-    public static function images()
+    public static function images(): string
     {
         return self::resources().'images'.DIRECTORY_SEPARATOR;
     }
